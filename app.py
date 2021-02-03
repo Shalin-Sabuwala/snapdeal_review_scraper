5149931e2743933a8c17b0a2e06126e7b48ff252
Shalin <sabuwalashalin@gmail.com>
2021-02-03 22:25:57 +0530

    revert changes

--- app.py
+++ app.py
@@ -41,19 +41,19 @@ def scrip():
 
                 productLink = str(box['href'])
 
-                # options = Options()
-                # options.add_argument('--headless')  # background task; don't open a window
-                # options.add_argument('--disable-gpu')
-                # options.add_argument('--no-sandbox')  # I copied this, so IDK?
-                # options.add_argument('--disable-dev-shm-usage')
-                # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
-                chrome_options = webdriver.ChromeOptions()
-                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-                chrome_options.add_argument("--headless")
-                chrome_options.add_argument("--disable-dev-shm-usage")
-                chrome_options.add_argument("--no-sandbox")
-                driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
-                                          chrome_options=chrome_options)
+                options = Options()
+                options.add_argument('--headless')  # background task; don't open a window
+                options.add_argument('--disable-gpu')
+                options.add_argument('--no-sandbox')  # I copied this, so IDK?
+                options.add_argument('--disable-dev-shm-usage')
+                driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+                # chrome_options = webdriver.ChromeOptions()
+                # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                # chrome_options.add_argument("--headless")
+                # chrome_options.add_argument("--disable-dev-shm-usage")
+                # chrome_options.add_argument("--no-sandbox")
+                # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+                #                           chrome_options=chrome_options)
 
                 driver.get(productLink)
 
